314b96a52581d14f9b4b33acc0ae24a2cddc75ee
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-01-08 12:24:48 -0800

    Added application.js file, but it doesn't seem to be working. Changed tweet generator to generate tweets less frequently.

---
+++ application.js
@@ -0,0 +1,23 @@
+
+// $(document).ready(function(){
+//   var $body = $('body');
+//   $body.html(''); 
+//   var index = streams.home.length - 1;
+//   while(index >= 0){
+//     var tweet = streams.home[index];
+//     var $tweet = $('<div></div>');
+//     $tweet.text('@' + tweet.user + ': ' + tweet.message);
+//     $tweet.appendTo($body);
+//     index -= 1;
+//   }
+// });
+
+$(document).ready(function() {
+  var index = streams.home.length -1;
+  while(index >=0){
+    var tweet = streams.home[index];
+    var $tweet = $('<li></li>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.appendTo($('.tweetlist'));
+  }
+});
