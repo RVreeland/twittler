77db89ddf2bb48ad1f6d9556eff09faeb010c80d
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-01-08 14:46:12 -0800

    Populated the "Those of interest" list with the names of users being followed.

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function() {
 
+//Display tweets on document load. Refresh button displays only new tweets. 
   var displayNew = function() {
     var lastTweet = -1; 
     var numTweets = streams.home.length - 1;
@@ -21,6 +22,15 @@ $(document).ready(function() {
   $('.refresh_tweets').on('click', function() {
     displayNew();
   })
+
+//Populate "Those of interest" field with users. (following)
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    var $user = $('<li></li>');
+    $user.text(user);
+    $user.appendTo($('.following'));
+  }
+
 
 });
 
